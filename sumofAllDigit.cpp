097fe4d6d325f88ec44868e102ee160f10cc32ad
Chan <85027277+Chan684@users.noreply.github.com>
2023-01-07 00:57:41 +0530

    Create sumofAllDigit.cpp

---
+++ sumofAllDigit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,sum=0,n;
+  cout<<"How many no of digit"<<endl;
+  cin>>n;
+    cin>>a;
+    for(int i=0;i<3;i++){
+        sum+= a%10;
+        a/=10;
+        
+    }cout<<sum<<endl;
+    return 0;
+}
