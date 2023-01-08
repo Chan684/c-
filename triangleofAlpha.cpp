3933bb2671ad557ef9fb1dad317268533d35b43f
Chan <85027277+Chan684@users.noreply.github.com>
2023-01-08 11:12:23 +0530

    Create triangleofAlpha.cpp

---
+++ triangleofAlpha.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=5;i++){
+        for(int k=0;k<5-i;k++){
+            cout<<" ";
+        }
+        int no_of_char=2*i-1;
+        for(int j=0;j<no_of_char;j++){
+        cout<<(char)('A'+j);
+    }cout<<endl;
+}
+    for(int t=6;t<t-5;t++){
+        cout<<' ';
+    }
+    return 0;
+}
