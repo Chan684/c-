1b76f86bb4629e070eaefb34ba0adba5874396f2
Chan <85027277+Chan684@users.noreply.github.com>
2023-02-19 20:56:30 +0530

    Create lengthOfString.cpp

---
+++ lengthOfString.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int i;
+    char ch[] = "Welcome11111111111";
+    for( i = 0;ch[i]!='\0';i++){
+
+    }
+    cout<<i<<endl;
+    return 0;
+}
