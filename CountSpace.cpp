71b2e5133d2804c856b52d1ddd67c712f5129c55
Chan <85027277+Chan684@users.noreply.github.com>
2023-02-19 21:40:53 +0530

    Update CountSpace.cpp

--- CountSpace.cpp
+++ CountSpace.cpp
@@ -4,7 +4,7 @@ int main()
 { int count=1;
     char ch[] = "How are you";
     for(int i = 0;ch[i]!='\0';i++){
-        if(ch[i]==' ') 
+        if(ch[i]==' '&& ch[i-1]!=' ') // if there are continuous space or white space
             count++;
     }
     cout<<count<<endl;
